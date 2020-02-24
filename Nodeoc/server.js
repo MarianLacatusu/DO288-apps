f70814f01682fa2794c0b9aad2d80335fa4514ff
MarianLacatusu <marian.lacatusu@aut.pub.ro>
2020-02-24 12:23:23 +0200

    Update server.js

--- Nodeoc/server.js
+++ Nodeoc/server.js
@@ -1,16 +1,10 @@
 const apm = require('elastic-apm-node').start({
-
-  serviceName: '',
-
-  secretToken: '',
-
-  serverUrl: '',
+ // serviceName: '',
+ // secretToken: '',
+  serverUrl: 'http://9.212.159.114:8200',
 })
-
 const app = require('express')()
-
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
-
 app.listen(3000)
